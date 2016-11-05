9719463c69aa067b262badbf46c31b348da1f930
GAURAVMA <gauravmadhikari03@gmail.com>
2016-11-05 16:33:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,12 +8,9 @@ app.use(morgan('combined'));
 var count=0;
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'homepage.html'));
-});
-
-app.get('/old', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/page1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
 });
