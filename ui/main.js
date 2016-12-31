983b6f19c2f873a9b83f840829d974fcb2aca8ca
GAURAVMA <gauravmadhikari03@gmail.com>
2016-12-31 12:54:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,46 @@
 //console.log('Loaded!');
-var button=document.getElementById('counter');
-
-button.onClick=function(){
-    
-    
-    var request=new XMLHttpRequest();
-    
-    request.onreadystatechange=function(){
-        
-        if(request.readyState === XMLHttpRequest.DONE){
-            
-            if(request.status === 200){
-                
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
-            }
-            
-            
+function getCounter(){
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
         }
-        
-    };
-    
-    request.open('GET','http://gauravma.imad.hasura-app.io/counter',true);
-   request.send(null); 
-    
+    }
 };
+ request.open('GET', '/counter', true);
+    request.send(null);
+}
+
+
+
+getCounter();
+//var button=document.getElementById('counter');
+
+//button.onClick=function(){
+    
+    
+  //  var request=new XMLHttpRequest();
+    
+    //request.onreadystatechange=function(){
+        
+      //  if(request.readyState === XMLHttpRequest.DONE){
+            
+        //    if(request.status === 200){
+                
+          //      var counter=request.responseText;
+            //    var span=document.getElementById('count');
+             //   span.innerHTML=counter.toString();
+            //}
+            
+            
+        //}
+        
+    //};
+    
+    //request.open('GET','http://gauravma.imad.hasura-app.io/counter',true);
+   //request.send(null); 
+    
+//};
