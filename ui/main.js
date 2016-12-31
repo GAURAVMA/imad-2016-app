0e1845e2902a6a1320b7515ebf60e36ec1f61c0b
GAURAVMA <gauravmadhikari03@gmail.com>
2016-12-31 13:34:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,26 @@
 //console.log('Loaded!');
+
+function getCounter(){
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+};
+ request.open('GET', 'http://gauravma.imad.hasura-app.io/counter', true);
+    request.send(null);
+}
+
+
+
+
+
+getCounter();
+
 var a=document.getElementById('a');
 
 a.onclick=function(){
