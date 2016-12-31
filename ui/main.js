34ea6d25344c5d65ddb9f1f789c96b3eec579ded
GAURAVMA <gauravmadhikari03@gmail.com>
2016-12-31 11:26:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,28 @@
-console.log('Loaded!');
+//console.log('Loaded!');
+var button=document.getElementbyId("counter");
+
+button.onCLick=function(){
+    
+    
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+        
+        if(request.readyState==XMLHttpRequest.DONE){
+            
+            if(request.status==200){
+                
+                var counter=request.responseText;
+                var span=document.getElementbyId("count");
+                span.innerHTML=counter.toString();
+            }
+            
+            
+        }
+        
+    };
+    
+    request.open('GET','/ui/counter',true);
+   request.send(null); 
+    
+};
