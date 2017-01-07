47238b4013d799c9deff982a9d705f6d8af21565
GAURAVMA <gauravmadhikari03@gmail.com>
2017-01-07 17:16:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/db', function (req, res) {
         if(err){}
         else{
             
-            artcount=JSON.stringify(result.count);
+            artcount=JSON.stringify(result.count(*));
             res.send(artcount);
         }
     });
