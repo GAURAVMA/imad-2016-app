e40c34f382129d71f6c238128c826d502e687c82
GAURAVMA <gauravmadhikari03@gmail.com>
2017-01-07 18:59:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.get('/db', function (req, res) {
         if(err){}
         else{
             
-            artcount=Number(JSON.stringify(result.rows[0].count));
-         
+            artcount=parseInt(JSON.stringify(result.rows[0].count));
+         res.send(artcount.toString());
         }
         pool.query('SELECT * from artlist', function(err,result)      {
     if(err){}
@@ -45,7 +45,7 @@ app.get('/db', function (req, res) {
         }
         
         sendata=sendata+"</div>";
-       res.send(sendata);
+       
         sendata="";
         //res.send(JSON.stringify(result.rows[0].artname)+JSON.stringify(result.rows[1].artname)+JSON.stringify(result.rows[2].artname)+JSON.stringify(result.rows[3].artname));
         
