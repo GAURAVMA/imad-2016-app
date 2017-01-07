b37fdc22447f4adaddff3c9f02c82461953308d0
GAURAVMA <gauravmadhikari03@gmail.com>
2017-01-07 21:27:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,9 +89,8 @@ app.get('/cb', function (req, res) {
     });
     });
 
-var comment=document.getElementById('comment').value;
-var username=document.getElementById('username').value;
-app.get('/cb1', function (req, res) {
+
+app.get('/cb1/:comment/:username', function (req, res) {
   pool.query('Insert into comlist (comment,username) values('+comment+','+username+')',function(err,result){
       if(err){}
       else{
